d7bf465b2adb9f3ea7ea115228508c04bb9f151b
mlisovyi <mlisovyi@gmail.com>
2020-01-26 21:38:23 +0100

    add neptune access credentials

--- fancy_model.py
+++ fancy_model.py
@@ -9,13 +9,21 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+import neptune
 
 parser = argparse.ArgumentParser(description='Train a ML model.')
 parser.add_argument('--n_estimators', type=int, default=10,
                     help='Number of trees in the forest.')
 parser.add_argument('--max_depth', type=int, default=2,
                     help='Number of lavels in a tree.')
+parser.add_argument('--token_file', type=str, default='~/.ssh/neptune.creds',
+                    help='The file containing neptune access token.')
 args = parser.parse_args()
+
+
+with open(agrs.token_file) as f:
+    neptune_token, _ = f.read().split('\n')
+neptune.init('mlisovyi/ibm_unconference2020_demo', api_token=neptune_token)
 
 # get the data
 data = load_breast_cancer()
