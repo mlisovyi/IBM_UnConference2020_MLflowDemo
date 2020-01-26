0b5a246e47bb34c0ba58b27a83abb316d026ef04
mlisovyi <mlisovyi@gmail.com>
2020-01-26 16:51:42 +0100

    add commadn-line control

--- fancy_model.py
+++ fancy_model.py
@@ -1,5 +1,5 @@
 # %%
-
+import argparse
 import joblib
 import datetime
 
@@ -12,23 +12,26 @@ import numpy as np
 import mlflow
 import mlflow.sklearn
 
+parser = argparse.ArgumentParser(description='Train a ML model.')
+parser.add_argument('--n_estimators', type=int, default=10,
+                    help='Number of trees in the forest.')
+parser.add_argument('--max_depth', type=int, default=2,
+                    help='Number of lavels in a tree.')
+args = parser.parse_args()
+
 # get the data
 data = load_breast_cancer()
 X, y = data['data'], data['target']
 X_trn, X_tst, y_trn, y_tst = train_test_split(X, y, test_size=0.2, random_state=314)
 
 # %%
-# model parameters
-n_estimators = 10
-max_depth = 2
-
 mlflow.set_experiment('Very random forest of trees')
 with mlflow.start_run():
-    mlflow.log_param('n_estimators', n_estimators)
-    mlflow.log_param('max_depth', max_depth)
+    mlflow.log_param('n_estimators', args.n_estimators)
+    mlflow.log_param('max_depth', args.max_depth)
 
     # train a model
-    mdl = RandomForestClassifier(n_estimators=n_estimators,  max_depth=max_depth, random_state=42)
+    mdl = RandomForestClassifier(n_estimators=args.n_estimators,  max_depth=args.max_depth, random_state=42)
     mdl.fit(X_trn, y_trn)
 
     # make predictions on hold-out dataset
